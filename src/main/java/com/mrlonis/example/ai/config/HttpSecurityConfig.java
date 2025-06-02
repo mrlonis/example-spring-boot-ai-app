2440351aa16cdd2365e7fcfcd91dec2f643e84c0
Matthew Lonis <mattlonis@gmail.com>
2025-06-01 23:22:26 -0400

    More setup

---
+++ src/main/java/com/mrlonis/example/ai/config/HttpSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.mrlonis.example.ai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class HttpSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
+    }
+}
